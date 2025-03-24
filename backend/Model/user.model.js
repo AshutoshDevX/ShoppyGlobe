60427b814fb3199358416f60a97a58657e5c04cc
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-03-24 16:24:53 +0530

    Update Model

--- backend/Model/user.model.js
+++ backend/Model/user.model.js
@@ -2,6 +2,16 @@ import mongoose from 'mongoose';
 
 mongoose.connect("mongodb+srv://admin:<Password>@cluster1.27bkq.mongodb.net/e-commerce");
 
+const db = mongoose.connection;
+
+db.on('open',()=>
+    console.log("Connection Successful")
+)
+
+db.on('error',()=>{
+    console.log("Connection not successful")
+})
+
 
 const user = mongoose.Schema({
     username:{
